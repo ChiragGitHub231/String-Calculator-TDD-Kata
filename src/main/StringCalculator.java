a8fbe60c2eb26ad3aebfad4fa7c953c0c9f6f459
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-08-01 17:17:16 +0530

    Write Test Case for String which contains more than 1 number separated by comma and returns it sum

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -4,8 +4,17 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0; // Return 0 for an empty string
+        } else if (numbers.length() == 1) {
+            int sum = numbers.charAt(0) - '0';
+            return sum;
         }
-        int sum = numbers.charAt(0) - '0';
+        // If there is more than 1 number like (1, 2, 3, 4)
+        int sum = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (numbers.charAt(i) - '0' >= 0 && numbers.charAt(i) - '0' <= 9) {
+                sum += numbers.charAt(i) - '0';
+            }
+        }
         return sum;
     }
 
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -14,4 +14,10 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(1, calculator.add("1"));
     }
+
+    @Test
+    public void testTwoNumbersReturnsSum() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+    }
 }
